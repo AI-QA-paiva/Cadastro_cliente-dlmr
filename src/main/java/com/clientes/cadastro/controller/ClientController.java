2eb495ba6d9d2bc01a5e15cec0a539171cdbd2d6
Bowie <marketing@letscode.com.br>
2025-01-07 18:40:22 -0300

    Subindo implementação da documentação com o Swagger

--- src/main/java/com/clientes/cadastro/controller/ClientController.java
+++ src/main/java/com/clientes/cadastro/controller/ClientController.java
@@ -2,6 +2,9 @@ package com.clientes.cadastro.controller;
 
 import com.clientes.cadastro.model.Client;
 import com.clientes.cadastro.service.ClientService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -17,6 +20,9 @@ public class ClientController {
     @Autowired
     private ClientService clientService;
 
+
+    @Operation(description = "Criará um novo cadastro para um novo cliente na base de dados")
+    @ApiResponse(responseCode = "201", description = "Retornará que o Cliente foi cadastrado e o id respectivo na na base de dados")
     @PostMapping(path="/client")
     public ResponseEntity<Client> creatClient (@Valid @RequestBody Client client){
 
@@ -24,28 +30,38 @@ public class ClientController {
         return new ResponseEntity<>(newClient, HttpStatus.CREATED);
     }
 
+
+    @Operation(description = "Retornará uma lista de clientes que estão cadastrados na base de dados")
+    @ApiResponse(responseCode = "200", description = "Retornará que a lista foi encontrada com sucesso na base de dados")
     @GetMapping(path = "/clients")
     public ResponseEntity<List<Client>> listClients (){
         return ResponseEntity.ok(clientService.searchForClients());
     }
 
+
+    @Operation(description = "Retornará os dados do cliente, conforme o seu Id cadastrado na base de dados")
+    @ApiResponse(responseCode = "200", description = "Retornará os dados do cliente conforme estiver cadastrado na base de dados")
     @GetMapping(path = "/client/{id}")
     public ResponseEntity<Optional<Client>> customerInformationId (@PathVariable Long id){
         return ResponseEntity.ok(clientService.getOnlyOneClient(id));
     }
 
+
+    @Operation(description = "Alterará algum dado do cliente ajustando/atualizando sua informação na base de dados")
+    @ApiResponse(responseCode = "200", description = "Retornará os dados ajustados/alterados do cliente com base no seu id cadastrado na base de dados")
     @PutMapping(path = "/client/{id}")
     public ResponseEntity<Client> customerRegistrationAdjustment(@RequestBody Client client, @PathVariable Long id){
         client.setId(id);
         return ResponseEntity.ok(clientService.registeredClient(client));
     }
 
+
+    @Operation(description = "Excluirá o cliente com base no Id, retirando seus dados da base de dados")
+    @ApiResponse(responseCode = "200", description = "Retornará que a operação de exclusão do cliente na base de dados foi executada")
     @DeleteMapping(path = "/client/{id}")
     public ResponseEntity<Void> deleteClient(@PathVariable Long id){
         clientService.deleteCustomerRecord(id);
         return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
     }
-
-
 
 }
